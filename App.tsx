42532cb1458fb7695e9f157898a39343b04b7e19
Royce Chua <royce236@gmail.com>
2022-07-05 15:26:20 +0800

    Add the gallery api, reducers, and components

--- App.tsx
+++ App.tsx
@@ -1,13 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useCallback, useEffect, useReducer } from 'react';
+import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
+import { getList } from './src/api/picsum';
+import PhotoGrid from './src/components/PhotoGrid';
+import { reducer, initialState, actionCreators } from './src/reducers/reducers';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  const { photos, nextPage, loading, error } = state
+
+  const fetchPhotos = useCallback(async () => {
+    dispatch(actionCreators.loading())
+
+    try {
+      const nextPhotos = await getList(nextPage)
+      dispatch(actionCreators.success(nextPhotos, nextPage))
+    } catch (e) {
+      dispatch(actionCreators.failure())
+    }
+  }, [nextPage])
+
+  useEffect(() => {
+    fetchPhotos()
+  }, [])
+
+
+  // We'll show an error only if the first page fails to load
+  if (photos.length === 0) {
+    if (loading) {
+      return (
+        <View style={styles.container}>
+          <ActivityIndicator animating={true} />
+        </View>
+      )
+    }
+
+    if (error) {
+      return (
+        <View style={styles.container}>
+          <Text>Failed to load photos!</Text>
+        </View>
+      )
+    }
+  }
+
+  return <PhotoGrid numColumns={3} photos={photos} onEndReached={fetchPhotos} />
 }
 
 const styles = StyleSheet.create({
